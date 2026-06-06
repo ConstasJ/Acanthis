e91b17155fff5615d215e4a1c83665470a92dac8
ConstasJ <constasj@qq.com>
2026-06-07 07:04:30 +0800

    chore: 基本完成了libraries部分的配置

---
+++ tools/config-tsdown/index.ts
@@ -0,0 +1,13 @@
+import type { UserConfig } from "tsdown";
+import { defineConfig } from "tsdown";
+
+export const baseConfig: UserConfig = defineConfig({
+	entry: ["./src/index.ts"],
+	outDir: "dist",
+	platform: "node",
+	target: "es2022",
+	dts: true,
+	clean: true,
+	sourcemap: true,
+	report: 'ci-only',
+});
