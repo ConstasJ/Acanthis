2d3e6697ccd53daa7d06f5ebd4693c9473e0d2d6
ConstasJ <constasj@qq.com>
2026-06-09 17:39:03 +0800

    feat(storage): 定义了sqlite表结构

---
+++ packages/storage/src/table.ts
@@ -0,0 +1,124 @@
+import { defineRelations } from "drizzle-orm";
+import {
+	integer,
+	primaryKey,
+	sqliteTable,
+	text,
+} from "drizzle-orm/sqlite-core";
+
+export const novels = sqliteTable("novels", {
+	id: integer("id").primaryKey({ autoIncrement: true }),
+	platform: text("platform").notNull(),
+	platformId: text("platform_id").notNull(),
+	name: text("name").notNull(),
+	author: text("author").notNull(),
+	summary: text("summary").notNull(),
+	cover: text("cover").notNull(),
+	status: text("status", {
+		enum: ["ongoing", "completed", "unknown"],
+	}).notNull(),
+});
+
+export const genres = sqliteTable("genres", {
+	id: integer("id").primaryKey({ autoIncrement: true }),
+	name: text("name").notNull().unique(),
+});
+
+export const novelGenres = sqliteTable(
+	"novel_genres",
+	{
+		novelId: integer("novel_id")
+			.notNull()
+			.references(() => novels.id, { onDelete: "cascade" }),
+		genreId: integer("genre_id")
+			.notNull()
+			.references(() => genres.id, { onDelete: "cascade" }),
+	},
+	(table) => [primaryKey({ columns: [table.novelId, table.genreId] })],
+);
+
+export const volumes = sqliteTable("volumes", {
+    id: integer("id").primaryKey({ autoIncrement: true }),
+    novelId: integer("novel_id")
+        .notNull()
+        .references(() => novels.id, { onDelete: "cascade" }),
+    name: text("name").notNull(),
+    platformId: text("platform_id").notNull(),
+});
+
+export const chapters = sqliteTable("chapters", {
+    id: integer("id").primaryKey({ autoIncrement: true }),
+    novelId: integer("novel_id")
+        .notNull()
+        .references(() => novels.id, { onDelete: "cascade" }),
+    volumeId: integer("volume_id")
+        .notNull()
+        .references(() => volumes.id, { onDelete: "cascade" }),
+    platformId: text("platform_id").notNull(),
+    name: text("name").notNull(),
+});
+
+export const keywordSearchves = sqliteTable("keyword_searches", {
+    keyword: text("keyword").primaryKey(),
+    queryTime: integer("query_time").notNull(),
+    total: integer("total").notNull(),
+});
+
+export const keywordNovels = sqliteTable(
+    "keyword_novels",
+    {
+        keyword: text("keyword")
+            .notNull()
+            .references(() => keywordSearchves.keyword, { onDelete: "cascade" }),
+        novelId: integer("novel_id")
+            .notNull()
+            .references(() => novels.id, { onDelete: "cascade" }),
+    },
+    (table) => [primaryKey({ columns: [table.keyword, table.novelId] })],
+);
+
+export const coverMetadata = sqliteTable("cover_metadata", {
+    hash: text("hash").primaryKey(),
+    contentType: text("content_type").notNull(),
+    originalUrl: text("original_url").notNull(),
+    ext: text("ext").notNull(),
+});
+
+export const cookies = sqliteTable("cookies", {
+    id: integer("id").primaryKey({ autoIncrement: true }),
+    domain: text("domain").notNull(),
+    path: text("path").notNull(),
+    name: text("name").notNull(),
+    value: text("value").notNull(),
+});
+
+export const generalCache = sqliteTable("general_cache", {
+    key: text("key").primaryKey(),
+    value: text("value").notNull(),
+});
+
+export const relations = defineRelations({ novels, genres, novelGenres, volumes, chapters, keywordSearchves, keywordNovels }, 
+    (r) => ({
+        novels: {
+            genres: r.many.genres({
+                from: r.novels.id.through(r.novelGenres.novelId),
+                to: r.genres.id.through(r.novelGenres.genreId),
+            }),
+            volumes: r.many.volumes(),
+        },
+        genres: {
+            novels: r.many.novels(),
+        },
+        volumes: {
+            novel: r.one.novels(),
+            chapters: r.many.chapters(),
+        },
+        chapters: {
+            novel: r.one.novels(),
+            volume: r.one.volumes(),
+        },
+        keywordSearchves: {
+            novels: r.many.novels(),
+        }
+    })
+)
