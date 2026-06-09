b2d197dbe3065a24ac4337db42d1a1fee48d626c
ConstasJ <constasj@qq.com>
2026-06-09 14:36:07 +0800

    feat(storage): 定义Zod Schema

--- packages/storage/src/types.ts
+++ packages/storage/src/types.ts
@@ -1,36 +1,64 @@
+import type { NovelStatus } from "@acanthis-dec/core";
+
+export type NovelTable = {
+	id: string;
+	title: string;
+	author?: string | null | undefined;
+	summary?: string | null | undefined;
+	cover?: string | null | undefined;
+	status?: NovelStatus | null | undefined;
+};
+
+export type GenreTable = {
+	id: number;
+	name: string;
+};
+
+export type NovelGenreTable = {
+	novelId: string;
+	genreId: number;
+};
+
+export type VolumeTable = {
+	id: number;
+	novelId: string;
+	title: string;
+};
+
+export type ChapterTable = {
+	id: string;
+	novelId: string;
+	volumeId: string;
+	name: string;
+};
+
 export type KeywordSearch = {
-    keyword: string;
-    queryTime: number;
-    total: number;
-}
+	keyword: string;
+	queryTime: number;
+	total: number;
+};
 
 export type KeywordNovel = {
-    keyword: string;
-    novelId: string[];
-}
-
-export type ChapterCache = {
-    id: string;
-    novelId: string;
-    name: string;
-}
+	keyword: string;
+	novelId: string[];
+};
 
 export type CoverMetadata = {
-    hash: string;
-    contentType: string;
-    originalUrl: string;
-    ext: string;
-}
+	hash: string;
+	contentType: string;
+	originalUrl: string;
+	ext: string;
+};
 
 export type GeneralCache = {
-    key: string;
-    value: string;
-}
+	key: string;
+	value: string;
+};
 
 export type CookieCache = {
-    id: number;
-    domain: string;
-    path: string;
-    name: string;
-    value: string;
-}
+	id: number;
+	domain: string;
+	path: string;
+	name: string;
+	value: string;
+};
