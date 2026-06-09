aa08d94c83055a0552d4f3a171ef51d63eb6d947
ConstasJ <constasj@qq.com>
2026-06-09 13:19:44 +0800

    refactor(browser-fetch): 整理模块整体export

--- packages/browser-fetch/src/index.ts
+++ packages/browser-fetch/src/index.ts
@@ -1,37 +1,10 @@
 export {
-	type AutoSolveDetectorType,
-	type AutoSolvePolicy,
-	type ChallengeDetector,
-	type ChallengeOptions,
-	detectCloudflareBlock,
-	detectCloudflareChallenge,
-	getDetector,
-	solveCloudflareChallenge,
-} from "./challenge.js";
-export {
 	type BinaryResponse,
 	BrowserFetchClient,
 	type BrowserFetchClientOptions,
 	type BrowserFetchRequest,
 	type BrowserFetchResponse,
 } from "./client.js";
-export {
-	type CookieStore,
-	type CookieStoreOptions,
-	type CookieStoreType,
-	FileCookieStore,
-	InMemoryCookieStore,
-} from "./cookies.js";
-export {
-	FlareSolverrClient,
-	type FlareSolverrOptions,
-} from "./flaresolverr.js";
-export {
-	type BrowserProfile,
-	type BrowserProfileName,
-	browserProfiles,
-	parseBrowserProfile,
-} from "./profile.js";
 export type {
 	TransportHttpOptions,
 	TransportRequest,
@@ -39,9 +12,3 @@ export type {
 	TransportSessionPolicy,
 	TransportTLSOptions,
 } from "./transport/types.js";
-export type { HttpMethod, ProxyOptions } from "./types.js";
-export {
-	iswwwFormUrlEncoded,
-	type WWWFormUrlEncodedBody,
-	wwwFormUrlEncodedToRecordStringString,
-} from "./utils.js";
