b2a5cbf0c594fb4885498c7aaa80fc28dde58392
ConstasJ <constasj@qq.com>
2026-06-10 18:04:33 +0800

    feat(storage): 确定index.ts导出

---
+++ packages/storage/src/index.ts
@@ -0,0 +1,11 @@
+export { DatabaseCookieStore } from "./cookies.js";
+export {
+	type DatabaseOptions,
+	DatabaseService,
+	type MigrationOptions,
+} from "./db.js";
+export { FSStorageService } from "./files.js";
+export {
+	StorageService,
+	type StorageServiceOptions,
+} from "./service.js";
