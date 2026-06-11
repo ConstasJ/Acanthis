e8dca02636417bba3aa53dabb0ee6bc81655bdff
ConstasJ <constasj@qq.com>
2026-06-11 09:29:22 +0800

    feat(browser-fetch): 将几个profile翻新为最新的chrome149

--- packages/browser-fetch/src/profile.ts
+++ packages/browser-fetch/src/profile.ts
@@ -1,7 +1,7 @@
 export type BrowserProfileName =
-	| "chrome146-linux"
-	| "chrome146-win"
-	| "chrome146-android";
+	| "chrome149-linux"
+	| "chrome149-win"
+	| "chrome149-android";
 
 export type BrowserProfile = {
 	name: BrowserProfileName;
@@ -15,23 +15,26 @@ export type BrowserProfile = {
 };
 
 export const browserProfiles: Record<BrowserProfileName, BrowserProfile> = {
-	"chrome146-linux": {
-		name: "chrome146-linux",
+	"chrome149-linux": {
+		name: "chrome149-linux",
 		userAgent:
-			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36",
+			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36",
 		headers: {},
+		impersonate: "chrome146",
 	},
-	"chrome146-win": {
-		name: "chrome146-win",
+	"chrome149-win": {
+		name: "chrome149-win",
 		userAgent:
-			"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36",
+			"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36",
 		headers: {},
+		impersonate: "chrome146",
 	},
-	"chrome146-android": {
-		name: "chrome146-android",
+	"chrome149-android": {
+		name: "chrome149-android",
 		userAgent:
-			"Mozilla/5.0 (Linux; Android 10; SM-G975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Mobile Safari/537.36",
+			"Mozilla/5.0 (Linux; Android 10; SM-G975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Mobile Safari/537.36",
 		headers: {},
+		impersonate: "chrome146",
 	},
 };
 
